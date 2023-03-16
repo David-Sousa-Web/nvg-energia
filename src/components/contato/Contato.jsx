ed42aec0bbf36c0b563d83570238b0996254d924
David Sousa <david01sousa@Hotmail.com>
2023-03-16 01:18:48 -0300

    terminando footer

--- src/components/contato/Contato.jsx
+++ src/components/contato/Contato.jsx
@@ -9,24 +9,18 @@ export default function Contato() {
   if (state.succeeded) {
       return (
         <div className='content-contato'>
-          
           <h1>Contato</h1>
-
           <div id="contato-container">
-
               <div className='contact-info'>
-
                 <h4>
                   Informação contato
                 </h4>
-
                 <p>preencha o formulário e eu entrarei em contato com você em até 24 horas</p>
-
                 <div className='icon-text'>
                   <i class="fa-solid fa-phone" ></i>
                   <span>(11) 98657-6360</span>
                 </div>
-                
+                            
                 <div className='icon-text'>
                   <i class="fa-solid fa-envelope" ></i>
                   <span>contato@nvgenergia.com.br</span>
@@ -38,7 +32,6 @@ export default function Contato() {
                 </div>
 
                 <div className='social-media'>
-
                   <a href='https://www.linkedin.com/in/david-sousa-dev/' className='icon-circle' target="_blank" rel="noreferrer">
                     <i class="fa-brands fa-linkedin-in"></i>
                   </a>
@@ -50,17 +43,12 @@ export default function Contato() {
                   <a href='https://www.instagram.com/david01sousa/' className='icon-circle' target="_blank" rel="noreferrer">
                     <i class="fa-brands fa-instagram"></i>
                   </a>
-                  
                 </div>
-
               </div>
-
               <div className='center-validation'>
-
                 <p id='message-validation'>Mensagem enviada com sucesso !</p>
 
               </div>
-
           </div>
         </div>
       );
@@ -68,17 +56,12 @@ export default function Contato() {
   return (
     <>  
         <section id='Contato' className='content-contato'>
-
             <h1>Contato</h1>
-
             <div id="contato-container">
-
               <div className='contact-info'>
-
                 <h4>
                   Informação contato
                 </h4>
-
                 <p>preencha o formulário e eu entrarei em contato com você em até 24 horas</p>
 
                 <div className='icon-text'>
@@ -95,9 +78,7 @@ export default function Contato() {
                   <i class="fa-solid fa-location-dot" ></i>
                   <span>Barueri, SP</span>
                 </div>
-
                 <div className='social-media'>
-
                   <a href='https://www.linkedin.com/in/david-sousa-dev/' className='icon-circle' target="_blank" rel="noreferrer">
                     <i class="fa-brands fa-linkedin-in"></i>
                   </a>
@@ -108,39 +89,24 @@ export default function Contato() {
 
                   <a href='https://www.instagram.com/david01sousa/' className='icon-circle' target="_blank" rel="noreferrer">
                     <i class="fa-brands fa-instagram"></i>
-                  </a>
-                  
+                  </a>                 
                 </div>
-
               </div>
-
               <form onSubmit={handleSubmit}>
-
                 <div className='form-part'>
-
                   <div className='form-group'>
-
                     <label>Nome Completo</label>
-
                     <input id='nome' type='text' name='Name' required="true"/>
-
                     <ValidationError
                       prefix='Nome'
                       field='name'
-                      errors={state.errors}
-                      
+                      errors={state.errors}                      
                     />
-
                   </div>
-
                 </div>
-
                 <div className='form-part'>
-
                   <div className='form-group'>
-
                     <label>E-mail</label>
-
                     <input id='email' type='email' name='email' required="true"/>
 
                     <ValidationError 
@@ -148,13 +114,9 @@ export default function Contato() {
                       field="email"
                       errors={state.errors}
                     />
-
                   </div>
-
-                  <div className='form-group'>
-                    
+                  <div className='form-group'>                   
                     <label>Telefone</label>
-
                     <input id='telefone' type='tel' name='telefone' required="true"/>
 
                     <ValidationError 
@@ -162,17 +124,11 @@ export default function Contato() {
                       field ="phone"
                       errors={state.errors}
                     />
-
                   </div>
-
                 </div>
-
                 <div className='form-part'>
-
                   <div className='form-group'>
-
                     <label>Mensagem</label>
-
                     <textarea id='message' name='message'/>
 
                     <ValidationError 
@@ -180,28 +136,18 @@ export default function Contato() {
                       field="message"
                       errors={state.errors}
                     />
-
                   </div>
-
                 </div>
-
                 <div className='col'>
-
                   <div className='form-group-button'>
-
                     <button className='primary' type="submit" disabled={state.submitting}>Enviar</button>
 
                   </div>
-
                 </div>
-
                 {/*<div className='message-send'>{result ? <Result/> : null}</div>*/}
-
               </form>
-
             </div>
-
-        </section>
+         </section>
     </>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,6 +3,40 @@ import './Footer.css'
 
 export default function Footer() {
   return (
-    <footer className='footer-container'>Footer</footer>
+    <footer className='footer-container'>
+      <div>
+        <p className='logo-nvg'>NVG <strong className='logo-energia'>Energia</strong></p>
+        <p className='p-footer'>Nosso compromisso com a inovação nos leva a desenvolver tecnologias de energia limpa.</p>
+      </div>
+      <div className='footer-content'>
+        <div className='footer-sobre'>
+          <a href="" className='footer-sobre-content'>Sobre</a>
+          <a href="" className='footer-sobre-content'>projetos</a>
+          <a href="" className='footer-sobre-content'>inicio</a>
+        </div>
+        <div className='footer-sobre'>
+          <p className='footer-sobre-content'>termos</p>
+          <p className='footer-sobre-content'>política de privacidade</p>
+          <p className='footer-sobre-content'>Cookies</p>
+        </div>
+        <div className='footer-sobre'>
+          <p className='footer-sobre-content'>contate-nos</p>
+          <p className='footer-sobre-content'>contato@nvgenergia.com.br</p>
+          <div className='icons-container-footer'>
+            <a href='https://www.linkedin.com/in/david-sousa-dev/' className='icons-footer' target="_blank" rel="noreferrer">
+              <i class="fa-brands fa-twitter"></i>
+            </a>
+
+            <a href='https://github.com/David-Sousa-Web' className='icons-footer' target="_blank" rel="noreferrer">
+              <i class="fa-brands fa-facebook"></i>
+            </a>
+
+            <a href='https://www.instagram.com/david01sousa/' className='icons-footer' target="_blank" rel="noreferrer">
+              <i class="fa-brands fa-instagram"></i>
+            </a>   
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
